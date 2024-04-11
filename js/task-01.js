1bb24dd1a9980944764e924475adf3aad525e434
Liubov-Pysana <144674433+Liubov-Pysana@users.noreply.github.com>
2024-04-11 11:58:26 +0100

    1,2,3 ex are done

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 // З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
-// Порахує й виведе в консоль кількість категорій в ul#categories,
-//     тобто елементів li.item.
+// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+
 // Для кожного елемента li.item у списку ul#categories знайде й виведе в
 // консоль текст заголовка елемента(тегу < h2 >) і кількість елементів
 //  у категорії(усіх < li >, вкладених у нього).
@@ -13,4 +13,15 @@
 // Дані за кожною категорією були отримані й виведені в консоль у тілі циклу
 // або методу forEach()
 // У консолі має бути виведено наступне повідомлення:
-const firstWord = "hello";
+
+const ulCategories = document.querySelector("#categories");
+const liItems = ulCategories.querySelectorAll("li.item");
+
+console.log(`Number of categories ${liItems.length}`);
+liItems.forEach((liItem) => {
+    const title = liItem.querySelector("h2").innerText;
+    console.log(`Category: ${title}`);
+
+    const elements = liItem.querySelectorAll("li").length;
+    console.log(`Elements: ${elements}`);
+});
